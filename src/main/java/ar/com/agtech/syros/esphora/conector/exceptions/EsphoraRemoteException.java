6233b4e292f36b9561e95194d297011d37e2ab6c
Jorge Morando <jmorando@cuyum.com>
2012-06-04 17:31:51 -0300

    Se agrega manejo de excepción remota

---
+++ src/main/java/ar/com/agtech/syros/esphora/conector/exceptions/EsphoraRemoteException.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ */
+package ar.com.agtech.syros.esphora.conector.exceptions;
+
+/**
+ * @author Jorge Morando
+ *
+ */
+public class EsphoraRemoteException extends Exception {
+
+	private static final long serialVersionUID = 9097198287865132201L;
+
+	public EsphoraRemoteException(String message){
+		super(message);
+	}
+	
+}
